d2bfc540e6aca55c20955f088600c2f9670f3f80
Kourosh Rezaei <kouroshrezaei90@gmail.com>
2024-08-13 13:15:18 +0200

    Spectral Analysis (span) for funcnodes

---
+++ funcnodes_span/__init__.py
@@ -0,0 +1,13 @@
+import funcnodes as fn
+
+
+__version__ = "0.1.0"
+
+NODE_SHELF = fn.Shelf(
+    name="Spectral Analysis",
+    description="Spectral analysis for funcnodes",
+    nodes=[],
+    subshelves=[
+
+    ],
+)
