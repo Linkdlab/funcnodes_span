fd718bbc09f3c8f90d35cb96ac35761f5401920f
JulianKimmig <julian.kimmig@gmx.net>
2024-10-18 19:37:01 +0200

    funcnodes-lmfit

--- funcnodes_span/__init__.py
+++ funcnodes_span/__init__.py
@@ -4,6 +4,7 @@ from .normalization import NORM_NODE_SHELF as NORM
 from .smoothing import SMOOTH_NODE_SHELF as SMOOTH
 from .peak_analysis import PEAKS_NODE_SHELF as PEAK
 from .baseline import BASELINE_NODE_SHELF as BASELINE
+from funcnodes_lmfit import NODE_SHELF as LMFIT_NODE_SHELF
 
 __version__ = "0.2.5"
 
@@ -11,5 +12,5 @@ NODE_SHELF = fn.Shelf(
     name="Spectral Analysis",
     description="Spectral analysis for funcnodes",
     nodes=[],
-    subshelves=[NORM, SMOOTH, BASELINE, PEAK],
+    subshelves=[NORM, SMOOTH, BASELINE, PEAK, LMFIT_NODE_SHELF],
 )
