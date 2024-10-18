a4ae70137a30b1e89853c1da411cdf61795e5fb4
JulianKimmig <julian.kimmig@gmx.net>
2024-10-18 19:37:36 +0200

    custom flatfit baseline

--- funcnodes_span/baseline.py
+++ funcnodes_span/baseline.py
@@ -4,6 +4,9 @@ from exposedfunctionality import controlled_wrapper
 import numpy as np
 from typing import Optional, Tuple, Union, List
 import pybaselines
+from scipy import sparse
+from scipy.sparse.linalg import spsolve
+from scipy.signal import savgol_filter
 
 
 class CostFunction(fn.DataEnum):
@@ -611,8 +614,75 @@ def _psalsa(
     return baseline_corrected, baseline, params
 
 
+@fn.NodeDecorator(
+    "baselines.whittaker.flatfit", name="FlatFit", outputs=[{"name": "baseline"}]
+)
+def flatfit(data: np.ndarray, smoothness: float, p: float) -> np.ndarray:
+    """
+    FlatFit: least squares weighted by inverse scale of 1st and 2nd derivatives with smoothness penalty
+
+    Parameters
+    ----------
+
+    data: ArrayLike
+        1D data
+
+    smoothness: float
+        size of smoothness penalty
+
+    p: float
+        relative size of Savitzky-Golay filter
+
+    Returns
+    -------
+    NDArray
+        values that minimize the asymmetric squared error with smoothness penalty
+
+    Description
+    -----------
+
+    This routine finds vector `z` that minimized:
+
+    `(y-z).T @ W @ (y-z) + smoothness * z.T @ D.T @ D @ z`
+
+    where `W` is determined by slope and curvature at given point
+
+    """
+    y = np.array(data)
+    assert len(y.shape) == 1, "Incorrect data input shape for AsLS"
+    assert len(y) > 3, "At least 4 data points muts be provided for AsLS"
+
+    L = len(y)
+
+    # scale lambda to maintain invariance for sampling frequency
+    lamb = smoothness * L**4
+
+    # calculate w
+    filter_window = max(4, int(L * p))
+    slope = savgol_filter(y, filter_window, 3, deriv=1)
+    curvature = np.gradient(slope)
+    slope = slope**2
+    slope /= np.sum(slope)
+    curvature = curvature**2
+    curvature /= np.sum(curvature)
+
+    w = 1 / (slope + curvature + 1e-10)
+
+    # calculate baseline
+
+    D = sparse.diags([1, -2, 1], [0, -1, -2], shape=(L, L - 2))
+    H = lamb * D.dot(D.transpose())
+
+    W = sparse.spdiags(w, 0, L, L)
+    Z = W + H
+    z = spsolve(Z, w * y)
+
+    return z
+
+
 WHITTAKER_NODE_SHELF = Shelf(
     nodes=[
+        flatfit,
         _airpls,
         _arpls,
         _asls,
