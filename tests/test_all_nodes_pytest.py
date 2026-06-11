ac651bfe8a7059ef6288a91d1146b739df84f109
Kourosh <kouroshrezaei90@gmail.com>
2026-06-11 09:02:33 +0200

    remove the bugged test file

--- tests/test_all_nodes_pytest.py
+++
@@ -1,6 +0,0 @@
-from pytest_funcnodes import all_nodes_tested
-import funcnodes_span as fnmodule  # noqa
-
-
-def test_all_nodes_tested(all_nodes):
-    all_nodes_tested(all_nodes, fnmodule.NODE_SHELF, ignore=[])
